fc58db5db8c82a0ed3fd956bada0f16d5d6f1517
Santiago Valencia <111545309+s4ntiagor@users.noreply.github.com>
2024-11-15 20:05:28 -0500

    Update app_streamlit.py
    app finished

--- app_streamlit.py
+++ app_streamlit.py
@@ -4,8 +4,17 @@ import numpy as np
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+import streamlit as st  
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import plotly.graph_objects as go
 
 import streamlit as st
+
+st.set_page_config(layout="wide")
 
 st.markdown("""
     <style>
@@ -56,7 +65,7 @@ st.markdown("""
     <h1>Pronóstico de Generación de Energía Fotovoltaica De La Universidad Autónoma de Occidente Usando Modelos de IA</h1>
 """, unsafe_allow_html=True)
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "Datos Recolectados", "Modelo", "Predicción", "Sobre Nosostros"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "Datos Recolectados", "Modelo", "Predicción", "Referencias"])
 
 with tab1:
     col1, col2 = st.columns([3, 2])
@@ -418,10 +427,222 @@ with tab3:
             </div>
         """, unsafe_allow_html=True)
 
+###########PREDICCIÓN###########
+
 with tab4:
     st.header("Predicción")
     st.write("""
-        En esta sección, describe el proceso de limpieza de datos y las transformaciones que realizaste para preparar 
-        los datos antes de entrenar el modelo. Puedes mencionar pasos como la eliminación de valores nulos, 
-        normalización de datos, etc.
+        En esta sección, describe el proceso de limpieza de datos y las transformaciones que realizamos para preparar 
+        los datos antes de entrenar el modelo.
     """)
+    
+        # Configuración de la página de Streamlit
+
+    # Cargar el modelo entrenado
+    @st.cache_resource
+    def load_model_cache():
+        return load_model("best_modelLSTM.keras")
+
+    model_LSTM = load_model_cache()
+
+    # Cargar y preparar los datos
+    @st.cache_data
+    def load_data():
+        df = pd.read_csv("df_imputado.csv", index_col=0, parse_dates=True)
+        return df
+
+    # Convertir la serie en datos supervisados
+    def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+        n_vars = 1 if type(data) is list else data.shape[1]
+        df = pd.DataFrame(data)
+        cols, names = list(), list()
+        for i in range(n_in, 0, -1):
+            cols.append(df.shift(i))
+            names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+        for i in range(0, n_out):
+            cols.append(df.shift(-i))
+            if i == 0:
+                names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+            else:
+                names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+        agg = pd.concat(cols, axis=1)
+        agg.columns = names
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg
+
+    # Preparar los datos para la predicción
+    df = load_data()
+    DatosM = pd.DataFrame({'Generacion_(kWh)': df['Generacion_(kWh)']})
+
+    # Parámetro para seleccionar el tipo de predicción
+    prediccion_tipo = st.sidebar.selectbox("Seleccione el tipo de predicción", ["Predicción Completa", "Predicción de Próximas 13 Horas"])
+
+    train_size = int(len(DatosM) * 0.8)
+    test_data = DatosM.iloc[train_size:]
+    
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    test_data_normalized = scaler.fit_transform(test_data[['Generacion_(kWh)']])
+    seq_length = 13 * 14  # Longitud de secuencia de entrada
+
+    # Definir la longitud de salida (13 horas)
+    out_length = 13
+
+    # Convertir los datos de prueba en formato supervisado
+    data_test_LSTM = series_to_supervised(test_data_normalized, seq_length, out_length)
+    X_test_LSTM = data_test_LSTM.values[:, 0:seq_length]
+    X_test_LSTM = X_test_LSTM.reshape(X_test_LSTM.shape[0], X_test_LSTM.shape[1], 1)
+
+    if prediccion_tipo == "Predicción Completa":
+        # Realizar predicciones completas
+        y_test_LSTM = data_test_LSTM.values[:, seq_length:]
+        pred_LSTM = model_LSTM.predict(X_test_LSTM)
+        pred_LSTM = scaler.inverse_transform(pred_LSTM)
+        real_data = scaler.inverse_transform(y_test_LSTM)
+        pred_LSTM = np.clip(pred_LSTM, a_min=0, a_max=None)
+        
+        # Calcular las métricas de predicción
+        rmse = np.sqrt(mean_squared_error(real_data, pred_LSTM))
+        mse = mean_squared_error(real_data, pred_LSTM)
+        mae = mean_absolute_error(real_data, pred_LSTM)
+        r2 = r2_score(real_data, pred_LSTM)
+
+    else:
+        # Predicción de Próximas 13 Horas
+        input_seq = X_test_LSTM[0]  # Secuencia inicial
+        pred = model_LSTM.predict(input_seq.reshape(1, seq_length, 1))
+        pred_LSTM = scaler.inverse_transform(pred).reshape(-1, 1)
+        
+        # Calcular las métricas de predicción para las próximas 13 horas
+        real_data = test_data['Generacion_(kWh)'].values[:out_length].reshape(-1, 1)
+        rmse = np.sqrt(mean_squared_error(real_data, pred_LSTM))
+        mse = mean_squared_error(real_data, pred_LSTM)
+        mae = mean_absolute_error(real_data, pred_LSTM)
+        r2 = r2_score(real_data, pred_LSTM)
+
+    # Configuración de columnas para gráficas y cuadro de métricas
+    col1, col2 = st.columns([3, 1])
+
+    with col2:
+        st.markdown(
+            """
+            <div style="background-color:#66cdaa; padding: 15px; border-radius: 10px; margin-top: 170px; margin-left: 30px; width: 90%;">
+                <h3 style="color:white; text-align:center;">Métricas del Modelo</h3>
+                <p style="color:white; font-size:16px;">Root Mean Square Error (RMSE): <b>{:.2f}</b></p>
+                <p style="color:white; font-size:16px;">Mean Absolute Error (MAE): <b>{:.2f}</b></p>
+                <p style="color:white; font-size:16px;">R2 Score: <b>{:.2f}</b></p>
+                <p style="color:white; font-size:16px;">Mean Squared Error (MSE): <b>{:.2f}</b></p>
+            </div>
+            """.format(rmse, mae, r2, mse),
+            unsafe_allow_html=True
+        )
+
+    with col1:
+        st.title("Predicción de Generación de Energía con LSTM")
+
+        if prediccion_tipo == "Predicción Completa":
+            # Gráfico de predicción completa
+            datos_full = np.array(range(len(real_data)))
+            fig1 = go.Figure()
+            fig1.add_trace(go.Scatter(x=datos_full, y=real_data[:, 0],
+                                    mode='lines', line=dict(width=2, color='#145DA0'), 
+                                    name="Generación real"))
+            fig1.add_trace(go.Scatter(x=datos_full, y=pred_LSTM[:, 0],
+                                    mode='lines', line=dict(width=2, color='#EC7C30'), 
+                                    name="Generación predicha"))
+            fig1.update_layout(title='Predicción Completa',
+                            paper_bgcolor='white', plot_bgcolor='white',
+                            font=dict(color='black'),
+                            yaxis=dict(title='Generación (kWh)', color='black'),
+                            xaxis=dict(title='Tiempo (horas)', color='black'),
+                            legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1, font=dict(color='black')))
+            st.plotly_chart(fig1, use_container_width=True)
+            
+            st.write("**Descripción del Gráfico de Predicción Completa:** El modelo logra capturar tendencias generales de generación, acercándose en gran medida a los valores reales. Sin embargo, se observan discrepancias en momentos de cambios abruptos, posiblemente debido a variaciones en la radiación solar que el modelo no detecta con precisión. Este análisis refleja que el modelo es eficaz en condiciones estables, aunque podría mejorarse para gestionar variaciones extremas con mayor precisión.")
+
+            # Histograma de Errores
+            errores = real_data - pred_LSTM
+            fig3 = go.Figure(data=[go.Histogram(x=errores[:, 0], nbinsx=30, marker=dict(color='#2E86C1'))])
+            fig3.update_layout(
+                title='Distribución de Errores (Real - Predicho)',
+                paper_bgcolor='white', plot_bgcolor='white',
+                font=dict(color='black'),
+                xaxis=dict(title='Error (kWh)', color='black'),
+                yaxis=dict(title='Frecuencia', color='black')
+            )
+            st.plotly_chart(fig3, use_container_width=True)
+
+            st.write("**Distribución de Errores**: Este histograma muestra la distribución de los errores de predicción, calculados como la diferencia entre los valores reales y los valores predichos por el modelo (Real - Predicho). En el eje X se representa el error en kWh, y en el eje Y, la frecuencia de cada valor de error. La mayoría de los errores se agrupan alrededor del valor cero, lo que indica que el modelo en general tiene una buena precisión, ya que sus predicciones están cerca de los valores reales. Sin embargo, el hecho de que algunos errores se distribuyan hacia los lados positivos y negativos sugiere que el modelo a veces sobreestima y otras veces subestima los valores, aunque estos casos extremos son menos frecuentes. Esta visualización es útil para identificar si existe algún sesgo en el modelo; una distribución simétrica alrededor de cero, como la que vemos aquí, sugiere que el modelo no tiene un sesgo claro hacia sobreestimar o subestimar en promedio.")
+
+        else:
+            # Gráfico de predicción de próximas 13 horas sin etiquetas específicas
+            fig2 = go.Figure()
+            fig2.add_trace(go.Scatter(x=list(range(out_length)), y=real_data[:, 0],
+                                    mode='lines', line=dict(width=2, color='#145DA0'), 
+                                    name="Generación real"))
+            fig2.add_trace(go.Scatter(x=list(range(out_length)), y=pred_LSTM[:, 0],
+                                    mode='lines', line=dict(width=2, color='#EC7C30'), 
+                                    name="Generación predicha"))
+            fig2.update_layout(
+                title='Predicción para las Próximas 13 Horas',
+                paper_bgcolor='white', plot_bgcolor='white',
+                font=dict(color='black'),
+                yaxis=dict(title='Generación (kWh)', color='black', tickfont=dict(color='black')),
+                xaxis=dict(title='Horas', color='black'),
+                legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1, font=dict(color='black'))
+            )
+            st.plotly_chart(fig2, use_container_width=True)
+            
+            st.write("**Predicción para las Próximas 13 Horas**: Este gráfico muestra la predicción del modelo para las próximas 13 horas. El eje X representa las horas de predicción desde el inicio de la secuencia, y el eje Y representa la generación de energía en kilovatios hora (kWh). La línea azul muestra los valores reales de generación, mientras que la línea naranja representa las predicciones del modelo.")
+
+        # Gráfico de dispersión de valores reales vs. predichos
+        fig5 = go.Figure()
+        fig5.add_trace(go.Scatter(x=real_data[:, 0], y=pred_LSTM[:, 0],
+                                mode='markers', marker=dict(color='#FF7F0E'), name="Predicho vs Real"))
+        fig5.add_trace(go.Scatter(x=[0, 250], y=[0, 250],
+                                mode='lines', line=dict(dash='dash', color='black'), name="Línea Perfecta"))
+        fig5.update_layout(
+            title='Valores Reales vs Predichos',
+            paper_bgcolor='white', plot_bgcolor='white',
+            font=dict(color='black'),
+            xaxis=dict(title='Generación Real (kWh)', color='black'),
+            yaxis=dict(title='Generación Predicha (kWh)', color='black'),
+            legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1, font=dict(color='black'))
+        )
+        st.plotly_chart(fig5, use_container_width=True)
+
+        st.write("**Valores Reales vs Predichos**: La mayoría de los puntos se agrupan en torno a la línea perfecta, lo que indica un desempeño satisfactorio del modelo LSTM en la estimación de la generación de energía. Sin embargo, conforme aumentan los valores de generación de energía, se logra observar una mayor dispersión de los puntos, lo que podría sugerir que el modelo experimenta ligeras desviaciones.")
+
+with tab5:
+    st.markdown("""
+        <div style="background-color:#4B8E8D; padding: 20px; border-radius: 10px;">
+            <h2 style="color:white;">Referencias</h2>
+            <ul style="color:white; font-size:16px; line-height:1.8;">
+                <li>
+                    Rodriguez-Leguizamon, C. K., López-Sotelo, J. A., Cantillo-Luna, S., & López-Castrillón, Y. U. (2023). 
+                    PV power generation forecasting based on XGBoost and LSTM models. In <em>2023 IEEE Workshop on Power Electronics 
+                    and Power Quality Applications (PEPQA)</em> (pp. 1–6). 
+                    <a href="https://doi.org/10.1109/PEPQA59611.2023.10325757" style="color:#f1c40f;">https://doi.org/10.1109/PEPQA59611.2023.10325757</a>
+                </li>
+                <li>
+                    RatedPower. (2022). Renewable Energy and Solar Research Report: What’s in Store for 2022. 
+                    <a href="https://go.ratedpower.com/hubfs/Solar_Trends_Report_2022.pdf" style="color:#f1c40f;">https://go.ratedpower.com/hubfs/Solar_Trends_Report_2022.pdf</a>
+                </li>
+                <li>
+                    Sharadga, H., Hajimirza, S., & Balog, R. S. (2020). Time series forecasting of solar power generation 
+                    for large-scale photovoltaic plants. <em>Renewable Energy, 150</em>, 797–807. 
+                    <a href="https://doi.org/10.1016/j.renene.2019.12.131" style="color:#f1c40f;">https://doi.org/10.1016/j.renene.2019.12.131</a>
+                </li>
+                <li>
+                    Keddouda, A., Ihaddadene, R., Boukhari, A., Atia, A., Arıcı, M., Lebbihiat, N., & Ihaddadene, N. (2023). 
+                    Solar photovoltaic power prediction using artificial neural network and multiple regression considering ambient and operating conditions. 
+                    <em>Energy Conversion and Management, 288(117186)</em>, 117186. 
+                    <a href="https://doi.org/10.1016/j.enconman.2023.117186" style="color:#f1c40f;">https://doi.org/10.1016/j.enconman.2023.117186</a>
+                </li>
+                <li>
+                    Sostenible, C. (s/f). REPORTE DE SOSTENIBILIDAD. Campussostenible.org. Recuperado el 6 de septiembre de 2024, de 
+                    <a href="https://campussostenible.org/wp-content/uploads/2023/11/Reporte-de-Sostenibilidad-2022.pdf" style="color:#f1c40f;">https://campussostenible.org/wp-content/uploads/2023/11/Reporte-de-Sostenibilidad-2022.pdf</a>
+                </li>
+            </ul>
+        </div>
+    """, unsafe_allow_html=True)
